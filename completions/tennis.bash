9e8d5862af7fb52ecab34b790341f9e25ec462b1
Adam Doppelt <amd@gurge.com>
2026-01-25 22:44:21 -0800

    wip

--- completions/tennis.bash
+++ completions/tennis.bash
@@ -1,20 +1,22 @@
+# bail without bash-completion
+declare -F _init_completion >/dev/null || return 2>/dev/null
+
 _tennis() {
   local cur prev
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  _init_completion || return
 
-  case "${prev}" in
-    --color) COMPREPLY=( $(compgen -W "auto never always" -- "${cur}") ); return ;;
-    --theme) COMPREPLY=( $(compgen -W "auto dark light" -- "${cur}") ); return ;;
+  case "$prev" in
+    --color|-c) COMPREPLY=( $(compgen -W "auto never always" -- "$cur") ); return ;;
+    --theme)    COMPREPLY=( $(compgen -W "auto dark light"   -- "$cur") ); return ;;
     -t|--title) return ;;
   esac
 
-  if [[ ${cur} == -* ]]; then
-    COMPREPLY=( $(compgen -W "--color --theme -n --row-numbers -t --title" -- "${cur}") )
+  if [[ $cur == -* ]]; then
+    COMPREPLY=( $(compgen -W "--color --theme -n --row-numbers -t --title" -- "$cur") )
   else
-    COMPREPLY=( $(compgen -f -X '!*.csv' -- "${cur}") $(compgen -d -- "${cur}") )
-    [[ ${#COMPREPLY[@]} -eq 0 ]] && COMPREPLY=( $(compgen -f -- "${cur}") )
+    _filedir csv
+    [[ ${#COMPREPLY[@]} -eq 0 ]] && _filedir
   fi
 }
 
-complete -o nospace -F _tennis tennis
+complete -F _tennis tennis
